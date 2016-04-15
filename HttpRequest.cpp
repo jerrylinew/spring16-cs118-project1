b2bcea84749abea52b0f27c520490340d85999a4
Andy Shih <andyshih12@hotmail.com>
2016-04-15 13:44:22 -0700

    decode first line

--- HttpRequest.cpp
+++ HttpRequest.cpp
@@ -1,5 +1,11 @@
 #include <string>
 #include <vector>
+// TODO make header files and use proper includes
+
+enum HttpMethod {
+  GET,
+  POST
+};
 
 class HttpRequest: public HttpMessage {
 public:
@@ -9,16 +15,41 @@ public:
     /*
       Sample Http Request
 
-      GET /index.html HTTP/1.1\r\n
+      GET /index.html HTTP/1.1\r\n   <----   decode this
       User-Agent: Wget/1.15 (linux-gnu)\r\n
       Accept: * / * \r\n   <---- no space between * and / but added space to avoid messing up comment block
       Host: localhost:4000\r\n
       Connection: Keep-Alive\r\n
       \r\n
+
+
+      GET /index.html HTTP/1.1\r\n
      */
-    
-    
-    
+
+    //must begin with method
+    bool foundFirstSpace = false;
+    bool foundSecondSpace = false;
+    std::string method, url, version;
+    for (int i = 0; i < line.size(); i++) {
+
+      if (line[i] == ' ') {
+	if (!foundFirstSpace)
+	  foundFirstSpace = true;
+	else
+	  foundSecondSpace = true;
+
+	continue;
+      }
+      if (foundSecondSpace)
+	version += line[i];
+      else if (foundFirstSpace)
+	url += line[i];
+      else
+	method += line[i];
+    }
+    setMethod(method);
+    setUrl(url);
+    setVersion(version);
   }
   
   void setUrl(std::string url) {
@@ -26,7 +57,7 @@ public:
     if (url.size() >= 4 && url.substr(0,4) == "http")
       m_isAbsoluteUrl = true;
     else
-      m_isAbsoluterUrl = false;
+      m_isAbsoluteUrl = false;
   }
   std::string getUrl() {
     return m_url;
